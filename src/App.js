ad3ed14792e5964c0d7b6f5ba431a9c3defa2b77
Neal Deters <nealdeters@gmail.com>
2020-04-17 22:58:39 -0500

    replaced my mapbox setup with a 3rd party react component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.scss';
-import MapBox from './MapBox';
 import Articles from './Articles';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import ReactMapGL from 'react-map-gl';
 
 function Navigation() {
   return (
@@ -21,11 +21,36 @@ function Navigation() {
 }
 
 function App() {
+  // setup map
+  const [viewport, setViewport] = React.useState({
+    latitude: 38,
+    longitude: -98,
+    width: "100vw",
+    height: "90vh",
+    zoom: 4,
+    // style: 'mapbox://styles/mapbox/streets-v11'
+  });
+  const mapRef = React.useRef();
+
+  // load and prepare data
+  // get map bounds
+  // get clusters
+
+  // return map
   return (
     <React.Fragment>
       <Navigation />
-      <Articles />
-      <MapBox lat={38} lng={-98} />
+      <ReactMapGL
+        {...viewport}
+        maxZoom={20}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+        onViewportChange={newViewport => {
+          setViewport({ ...newViewport });
+        }}
+        ref={mapRef}
+      >
+        {/* markers here */}
+      </ReactMapGL>
     </React.Fragment>
   );
 }
